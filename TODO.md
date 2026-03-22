3e7783648649fb7c950a12ac0155d4e277937db5
vaughn <hauck.trevor@gmail.com>
2026-03-22 18:13:04 -0400

    User only moves 1 square on keypress

--- TODO.md
+++ TODO.md
@@ -6,7 +6,12 @@
 - [x] render all rows 
 - [x] register keypress, print debug msg on press 
 - [x] keypress moves player
-- [] When user presses key, it only moves them 1 square
-- [] Add bounds to where they can move
-- [] as a user goes over a square, the color changes
-
+- [x] When user presses key, it only moves them 1 square
+___
+- [ ] Add bounds to where they can move
+- [ ] as a user goes over a square, the color changes
+- [ ] if all the squares are changed, the level ends
+- [ ] level end state - fail and success
+- [ ] game intro state
+- [ ] if player goes over incorrect square - level end state fail
+- [ ] map 1
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -11,6 +12,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
+var userKeyPress = false
 var playerPosX = 0
 var playerPosY = 0
 var gameWidth = 600
@@ -23,21 +25,28 @@ type Game struct {
 
 func (g *Game) Update() error {
 	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+	// isInputting := false
+	// if len(g.keys) != 0 {
+	// 	isInputting = true
+	// } else {
+	// 	isInputting = false
+	// }
 
 	// fmt.Printf("g keys: %v \n", len(g.keys))
 	if len(g.keys) != 0 {
+
+		userKeyPress = true
 		key := fmt.Sprintf("%v", g.keys[0])
-		// ebitenutil.DebugPrint(screen, fmt.Sprintf("%v", g.keys[0]))
-		if key == "w" || key == "W" || key == "UpArrow" {
+		if key == "w" || key == "W" || key == "ArrowUp" {
 			playerPosY -= offsetVal
 		}
-		if key == "a" || key == "A" || key == "LeftArrow" {
+		if key == "a" || key == "A" || key == "ArrowLeft" {
 			playerPosX -= offsetVal
 		}
-		if key == "s" || key == "S" || key == "DownArrow" {
+		if key == "s" || key == "S" || key == "ArrowDown" {
 			playerPosY += offsetVal
 		}
-		if key == "d" || key == "D" || key == "RightArrow" {
+		if key == "d" || key == "D" || key == "ArrowRight" {
 			playerPosX += offsetVal
 		}
 	}
@@ -66,6 +75,14 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// draw player
 	vector.FillRect(screen, float32(playerPosX), float32(playerPosY), float32(sizeW), float32(sizeH), color.RGBA{255, 20, 80, 200}, false)
+
+	if userKeyPress == true {
+		time.Sleep(150 * time.Millisecond)
+		userKeyPress = false
+	}
+	if len(g.keys) != 0 {
+		ebitenutil.DebugPrint(screen, fmt.Sprintf("%v", g.keys[0]))
+	}
 
 }
 
